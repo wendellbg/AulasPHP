58ce7c417f659b37316734d3d0b8f6135522c828
Wendell <wendellbg@gmail.com>
2023-11-09 08:36:11 -0300

    Aulas de PHP
    PHP com MySQL - SELECT e INSERT

---
+++ aula17.php
@@ -0,0 +1,12 @@
+<?php
+include "conecta_mysql.inc";
+$codigo = $_POST["codigo"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+$unidade = $_POST["unidade"];
+$resultado = mysqli_query($conexao, "INSERT INTO produto VALUES ($codigo,'$descricao',$preco,'$unidade')");
+$linhas = mysqli_affected_rows($conexao);
+mysqli_close($conexao);
+echo "<p>";
+echo "Foram inseridos ".$linhas." registros";
+?>
