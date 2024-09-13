36dc2968ac1dec39898f2463807a6b26230db2ff
Wendell Bento Geraldes <wendellbg@gmail.com>
2024-09-13 09:43:26 -0300

    Alteração no arquivo
    alterei a mensagem do comando echo inserindo a tag <H1></H1>

--- OlaMundo.php
+++ OlaMundo.php
@@ -1,3 +1,3 @@
 <?php
-echo "Olá pessoal, isto é o o meu primeiro programa em PHP";
+	echo "<h1>Olá pessoal, isto é o o meu primeiro programa em PHP</h1>";
 ?>
---
+++ OlaMundo.php.bak
@@ -0,0 +1,3 @@
+<?php
+echo "Olá pessoal, isto é o o meu primeiro programa em PHP";
+?>
